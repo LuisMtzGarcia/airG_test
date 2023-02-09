5005367606a90e8af7a7827d926df5bfad341475
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2023-02-08 20:00:33 -0600

    First version of second test
    It's a basic function that writes a CSV with random data.

---
+++ airg_python_2.py
@@ -0,0 +1,20 @@
+import csv
+import random
+import string
+
+def generate_csv():
+    """Generates a CSV with random data."""
+
+    filename = "random_data.csv"
+
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+        for i in range(100):
+            random_number = str(random.randint(1, 10000))
+            random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+            writer.writerow([random_number, random_string])
+    
+    print(f"CSV file with random data has been generated and saved as {filename}")
+
+
+generate_csv()
