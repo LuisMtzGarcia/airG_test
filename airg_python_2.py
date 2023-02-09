76fa951e44c4f228b5e8ed53f96ee6f6379b3a20
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2023-02-08 20:15:52 -0600

    Refactor generate_csv() to work in chunks
    This change speeds up the writing, writes a CSV file with a million rows.

--- airg_python_2.py
+++ airg_python_2.py
@@ -7,12 +7,24 @@ def generate_csv():
 
     filename = input("Enter the filename: ").replace(" ", "_").replace(",", "_") + ".csv"
 
+    chunk_size = 10_000
+    total_rows = 1_000_000
+
+
     with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
-        for i in range(100):
-            random_number = str(random.randint(1, 10000))
-            random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-            writer.writerow([random_number, random_string])
+
+        # Starting point, endpoint, step size.
+        for i in range(0, total_rows, chunk_size):
+            rows = []
+            for j in range(chunk_size):
+                if i + j >= total_rows:
+                    break
+                random_number = str(random.randint(1, 10000))
+                choices_string = string.ascii_letters + string.digits
+                random_string = ''.join(random.choices(choices_string, k=10))
+                rows.append([random_number, random_string])
+            writer.writerows(rows)
     
     print(f"CSV file with random data has been generated and saved as {filename}")
 
