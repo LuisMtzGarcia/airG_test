6438cffaf70ae82f5be285dbc5fe122acc648e1e
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2023-02-08 19:43:49 -0600

    Create another function with list comprehension
    It has the same functionality.
    Also created a function to print the results, called print_results().

--- airg_python_1.py
+++ airg_python_1.py
@@ -13,6 +13,7 @@ def return_vehicles(page_number=1):
 
 
 def list_manufacturers():
+    """Returns a list of five unique manufacturers."""
 
     manufacturers = set()
     page_number = 1
@@ -28,4 +29,28 @@ def list_manufacturers():
     return list(sorted(manufacturers))
 
 
-print(list_manufacturers())
+def manufacturers_list_comprehension():
+    """Same logic as list_manufacturers() but done with list comprehension."""
+
+    manufacturers = set()
+    page_number = 1
+
+    while len(manufacturers) < 5:
+        results = return_vehicles(page_number)
+        manufacturers.update(
+            result["manufacturer"].strip().title() for result in results if len(manufacturers) < 5
+        )
+        page_number += 1
+
+    return sorted(manufacturers)
+
+
+def print_results():
+    manufacturers = list_manufacturers()
+    list_comprehension = manufacturers_list_comprehension()
+
+    print(f"Results: \n{manufacturers}")
+    print(f"Results with list comprehension: \n{list_comprehension}")
+
+
+print_results()
